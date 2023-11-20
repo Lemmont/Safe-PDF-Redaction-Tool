d2a4db66ed4cd4ed7ece91116acac7f74cfe81d8
Lennaert Feijtes <lennaert.feijtes@student.uva.nl>
2023-11-20 15:48:03 +0100

    added full functionality of the notebook to a class system in python

--- src/safePdfRedactionTool/documentInterpreter/__init__.py
+++ src/safePdfRedactionTool/documentInterpreter/__init__.py
@@ -1,13 +1,15 @@
 from typing import Dict
 import fitz
 import re
+import codecs
+
 class DocumentInterpreter:
     def __init__(self, document: fitz.Document, pages: Dict[int, fitz.Page], fonts: Dict[int, str]):
         self.doc: fitz.Document = document
-        self.pages: Dict[int, fitz.Page] = pages # TO BE EDITED
+        self.pages: Dict[int, any] = pages # TO BE EDITED
         self.fonts: Dict[int, Dict[str, any]] = fonts # TO BE EDITED
 
-    def parse_character_mappings(self, page_fonts: Dict[str, any]):
+    def parse_character_mapping(self, page_fonts: Dict[str, any]):
         """
             Get character mapping of fonts on page
 
@@ -38,3 +40,70 @@ class DocumentInterpreter:
             fonts_mapping[page_font[0]] = mapping
 
         return fonts_mapping
+
+    def parse_text_elements(self, text_contents):
+        """
+            Parse text elmentents previously obtained from a content stream in a page.
+
+            :param text_elements_content_stream: BT...ET text elements from content stream
+            :return: return dict where every text element is mapped to its position
+                    on the page.
+        """
+
+        text_elements = {}
+        pattern = re.compile(r'(\(.*?\))(\w+)')
+        for match in text_contents:
+            font_info = {'reference': '/F0', 'fontsize': '11', 'match': ""}
+            raw_string = ""
+            split = match.strip().split("\n")
+            for item in split:
+                tokenized = ""
+                if item[0] == "(":
+                    tokenized = [item]
+                else:
+                    tokenized = item.split(" ")
+
+                if tokenized[-1] == 'Tf':
+                    font_info['reference'] = tokenized[0]
+                    font_info['fontsize'] = tokenized[1]
+                    font_info['match'] = item
+                    continue
+                elif tokenized[-1] == 'Tm':
+                    x = float(tokenized[4])
+                    y = float(tokenized[5])
+                    raw_string = item
+                elif len(tokenized) == 1:
+                    try:
+                        pk = codecs.escape_decode(tokenized[0].strip())[0].decode('utf-8', errors='replace').replace('\\)', ')')
+                        # what if (..')'.'('...) ?
+                        pk = pattern.match(pk)
+                        # decode string here
+
+                        # check if pk[1] == 'Tj'
+                        text = pk.group(1)[1:-1]
+                        raw_string = raw_string + "\n" + item
+                        text_elements[(x, y)] = {'font': font_info, 'string': text, 'match': raw_string}
+                    except ValueError:
+                        pass
+
+        return text_elements
+
+    def translate_text_elements(self, text_elements, fonts_mapping):
+        """
+            Translate text elements of a content stream of a page using the
+            fonts character mapping from the page.
+        """
+        string = ""
+        for text in text_elements.values():
+            font_reference = text['font']['reference']
+            value = text['string']
+            for item in value[1:]:
+                # empty/space charachter
+                if item == "\x00 ":
+                    string = string + item
+                try:
+                    string = string + fonts_mapping[font_reference.split("/")[1]]["\x00" + item]
+                except:
+                    pass
+
+        return string
--- src/safePdfRedactionTool/safePdfRedactionTool.py
+++ src/safePdfRedactionTool/safePdfRedactionTool.py
@@ -14,7 +14,11 @@ def main():
     page_fonts = {6: reader.get_page_fonts(pagesDoc[6])}
 
     interpreter = DocumentInterpreter(reader.doc, pagesDoc, page_fonts)
-    #print(interpreter.parse_character_mappings(interpreter.fonts[6]))
+    cmap = interpreter.parse_character_mapping(interpreter.fonts[6])
+    content_stream = reader.get_content_from_stream(page_content[17])
+    text = interpreter.parse_text_elements(content_stream)
+    text2 = interpreter.translate_text_elements(text, cmap )
+    print(text2)
 
 # Using the special variable
 # __name__
